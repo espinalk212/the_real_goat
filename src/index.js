cbe5934c152bd9b35e75bd17d75c7ae92a1afe08
espinalk212 <espinalk212@gmail.com>
2020-08-19 20:08:44 -0400

    add styling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./styles/index.scss";
+import * as d3 from 'd3';
+import Chart from './scripts/chart';
+import LebronStats from './playerStats/lebron_season_stats.json'
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const svgChart = document.getElementById("#d3-container")
+  const chart = new Chart(svgChart, LebronStats)
+  chart
+})
+  
---
+++ src/scripts/chart.js
@@ -0,0 +1,72 @@
+import * as d3 from 'd3';
+import { svg } from 'd3';
+
+
+class Chart {
+  constructor(props, dataset) {
+
+    this.width = 1300;
+    this.height = 600;
+    this.margin = { top: 20, bottom: 20, right: 20, left: 35 }
+    this.svg = 
+      d3.select('#d3-container')
+      .append('svg')
+      .attr('height', this.height - this.margin.top - this.margin.bottom)
+      .attr('width', this.width - this.margin.left - this.margin.right)
+      .attr('viewBox', [0, 0, this.width, this.height ])
+
+    this.x = d3.scaleBand()
+      .domain(d3.range(dataset.data.length))
+      .range([this.margin.left, this.width - this.margin.right])
+      .padding(0.1)
+
+    this.y = d3.scaleLinear()
+      .domain([0, 70])
+      .range([this.height - this.margin.bottom, this.margin.top])
+
+
+    this.svg
+      .append('g')
+      .attr('fill', 'orange')
+      .selectAll('rect')
+      .data(dataset.data)
+      .join('rect')
+        .attr('x', (d, i) => this.x(i))
+        .attr('y', d => this.y(d.pts))
+        .attr('height', d => this.y(0) - this.y(d.pts))
+        .attr('width', this.x.bandwidth())
+        .attr('class', 'rect')
+
+    this.xAxis = (g) => {
+      g.attr('transform', `translate(0, ${this.height - this.margin.bottom})`)
+      .call(d3.axisBottom(this.x).tickFormat(i => dataset.data[i].game.date))
+      .attr('font-size','20px')
+    }
+
+    this.yAxis = (g) => {
+      g.attr("transform", `translate(${this.margin.left}, 0)`)
+      .call(d3.axisLeft(this.y).ticks(null, dataset.format))
+      .attr("font-size", "20px");
+    };
+
+    this.svg.append('g').call(this.xAxis)
+    this.svg.append('g').call(this.yAxis)
+    this.svg.node()
+  }
+  
+  
+
+
+
+
+
+
+
+    
+   
+  
+}
+
+
+export default Chart;
+
